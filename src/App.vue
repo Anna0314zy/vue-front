5dd25b4e3212c3677f410cea4851606ad4a99ccb
Zoe <891374900@qq.com>
2020-10-27 14:39:49 +0800

    y

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+   <ul>
+     <li v-for="user in users" :key=user.id>
+       {{user}}
+     </li>
+   </ul>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/api/users').then(res => {
+      this.users = res.data;
+    })
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
